35cb4d6cce2561926d0920ddce0065825ac5b393
valetzu <valzerlazer@gmail.com>
2022-11-17 21:12:20 +0200

    Fixed a bug where player2 would only have an advantage with more than 4 points. Fixed by adding = operator to the if statement. Also added tests to 100% cover all the Tennis game code.

--- tests/TennisGameTest.java
+++ tests/TennisGameTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 public class TennisGameTest {
 	
+	
 // Here is the format of the scores: "player1Score - player2Score"
 // "love - love"
 // "15 - 15"
@@ -61,5 +62,101 @@ public class TennisGameTest {
 		//Act
 		// This statement should cause an exception
 		game.player1Scored();			
-	}		
-}
+	}	
+	
+	@Test (expected = TennisGameException.class)
+	public void testTennisGame_Player2WinsPointAfterGameEnded_ResultsException() throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();
+		//Act
+		game.player2Scored();
+		game.player2Scored();
+		game.player2Scored();
+		game.player2Scored();
+		//Act
+		// This statement should cause an exception
+		game.player2Scored();			
+	}	
+	
+	@Test 
+	public void testTennisGame_GameResults_p1_wins() throws TennisGameException{
+		TennisGame game = new TennisGame();
+		
+		game.player1Scored();
+		game.player1Scored();
+		game.player1Scored();
+		game.player1Scored();
+		
+		assertEquals("player1 wins", game.getScore());
+	}
+	
+	@Test
+	public void testTennisGame_GameResults_p2_wins() throws TennisGameException{
+		TennisGame game = new TennisGame();
+		
+		game.player2Scored();
+		game.player2Scored();
+		game.player2Scored();
+		game.player2Scored();
+		
+		assertEquals("player2 wins", game.getScore());
+	}
+	
+	@Test
+	public void testTennisGame_player1_has_advantage_4_3() throws TennisGameException{
+		TennisGame game = new TennisGame();
+		
+		game.player1Scored();
+		game.player1Scored();
+		game.player1Scored();
+		game.player2Scored();
+		game.player2Scored();
+		game.player2Scored();
+		game.player1Scored();
+		assertEquals("player1 has advantage", game.getScore());
+	}
+	
+	@Test
+	public void testTennisGame_player2_has_advantage_4_3() throws TennisGameException{
+		TennisGame game = new TennisGame();
+		
+		game.player2Scored();
+		game.player2Scored();
+		game.player2Scored();
+		game.player1Scored();
+		game.player1Scored();
+		game.player1Scored();
+		game.player2Scored();
+		assertEquals("player2 has advantage", game.getScore());
+	}
+	
+	@Test
+	public void testTennisGame_score0() throws TennisGameException{
+		TennisGame game = new TennisGame();
+		assertEquals("love - love", game.getScore());
+	}
+	
+	@Test
+	public void testTennisGame_score1() throws TennisGameException{
+		TennisGame game = new TennisGame();
+		game.player1Scored();
+		assertEquals("love - 15", game.getScore());
+	}
+	
+	@Test
+	public void testTennisGame_score2() throws TennisGameException{
+		TennisGame game = new TennisGame();
+		game.player1Scored();
+		game.player1Scored();
+		assertEquals("love - 30", game.getScore());
+	}
+	
+	@Test
+	public void testTennisGame_score3() throws TennisGameException{
+		TennisGame game = new TennisGame();
+		game.player1Scored();
+		game.player1Scored();
+		game.player1Scored();
+		assertEquals("love - 40", game.getScore());
+	}
+ }
